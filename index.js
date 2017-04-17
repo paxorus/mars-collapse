e961cc0312bec67f100e48d07f7e25ebfe709a14
paxorus <prakhar@brandeis.edu>
2017-04-17 02:11:55 -0400

    Reattempt: Deploy to Heroku

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -150,6 +151,6 @@ function generateUuid() {
 
 
 
-http.listen(5000, function () {
-	console.log('Node is listening on port 5000');
+http.listen(app.get('port'), function () {
+	console.log('Node is listening on port', app.get('port'));
 });
